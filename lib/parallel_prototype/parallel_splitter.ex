1bf83eab1054c3e817a9c1b56532c72820c5c63e
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-26 17:41:46 +0900

    add id to ParallelSplitter.split

--- lib/parallel_prototype/parallel_splitter.ex
+++ lib/parallel_prototype/parallel_splitter.ex
@@ -6,7 +6,7 @@ defmodule ParallelSplitter do
   @doc """
   Returns list of processes or tuples of a process and a reference 
   that spawns the given function `fun` from module `mod`, 
-  passing `pid` and a list containing `threshold` elements each,
+  passing `pid`, a list containing `threshold` elements each, and `id` of the list,
   according to the given options.
 
   The result depends on the given options. 
@@ -16,16 +16,16 @@ defmodule ParallelSplitter do
 
   It also accepts extra options, for the list of available options check `:erlang.spawn_opt/4`. 
   """
-  @spec split({module(), atom()}, pid(), Enum.t(), pos_integer(), Process.spawn_opts()) ::
+  @spec split({module(), atom()}, pid(), non_neg_integer(), Enum.t(), pos_integer(), Process.spawn_opts()) ::
           [pid() | {pid(), reference()}]
-  def split({_, _}, _, [], _, _), do: []
+  def split({_, _}, _, _, [], _, _), do: []
 
-  def split({mod, fun}, pid, enumerable, threshold, opts) do
+  def split({mod, fun}, pid, id, enumerable, threshold, opts) do
     {heads, tail} = Enum.split(enumerable, threshold)
 
     [
-      Process.spawn(mod, fun, [pid, heads], opts)
-      | split({mod, fun}, pid, tail, threshold, opts)
+      Process.spawn(mod, fun, [pid, heads, id], opts)
+      | split({mod, fun}, pid, id + 1, tail, threshold, opts)
     ]
   end
 end
