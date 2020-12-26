0a242807a70dd5b2aa6b5fd79e1e2383c07d548e
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-26 23:19:07 +0900

    add test pmap_chunk

--- test/parallel_prototype_test.exs
+++ test/parallel_prototype_test.exs
@@ -5,4 +5,8 @@ defmodule ParallelPrototypeTest do
   test "pmap" do
     assert ParallelPrototype.pmap(1..100, &(&1 * 2), 10) == Enum.map(1..100, &(&1 * 2))
   end
+
+  test "pmap_chunk" do
+  	assert ParallelPrototype.pmap_chunk(1..100, &(&1 * 2), fn x -> Enum.map(x, &(&1 * 2)) end, 10) == Enum.map(1..100, &(&1 * 2))
+  end
 end
