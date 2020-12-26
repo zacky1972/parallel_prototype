1ec59ea7eeeb200a5f2176dff8129ff09a38fc87
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-26 17:59:32 +0900

    add arg to ParallelSplitter.split

--- lib/parallel_prototype.ex
+++ lib/parallel_prototype.ex
@@ -2,4 +2,15 @@ defmodule ParallelPrototype do
   @moduledoc """
   Documentation for `ParallelPrototype`.
   """
+
+  @doc """
+  """
+  #@spec pmap(Enum.t(), (Enum.element() -> any()), pos_integer()) :: list()
+  #def pmap(enumerable, fun, threshold // 1000) do
+  #	ParallelSplitter.split({ParallelPrototype, :sub})
+  #end
+
+  #def sub(pid, enumerable, id) do
+  #	
+  #end
 end
--- lib/parallel_prototype/parallel_splitter.ex
+++ lib/parallel_prototype/parallel_splitter.ex
@@ -6,7 +6,7 @@ defmodule ParallelSplitter do
   @doc """
   Returns a reversed list of tuples of a process or tuple of a process and a reference, and identity of the process 
   that spawns the given function `fun` from module `mod`, 
-  passing `pid`, a list containing `threshold` elements each, and `id` of the list,
+  passing `pid`, a list containing `threshold` elements each, `id` of the list and `arg`,
   according to the given options.
 
   The result depends on the given options. 
@@ -16,14 +16,14 @@ defmodule ParallelSplitter do
 
   It also accepts extra options, for the list of available options check `:erlang.spawn_opt/4`. 
   """
-  @spec split({module(), atom()}, pid(), non_neg_integer(), Enum.t(), pos_integer(), Process.spawn_opts()) ::
+  @spec split({module(), atom()}, pid(), non_neg_integer(), Enum.t(), pos_integer(), any(), Process.spawn_opts()) ::
           [{pid(), non_neg_integer()} | {{pid(), reference()}, non_neg_integer()}]
-  def split({_, _}, _, _, [], _, _), do: []
+  def split({_, _}, _, _, [], _, _, _), do: []
 
-  def split({mod, fun}, pid, id, enumerable, threshold, opts) do
+  def split({mod, fun}, pid, id, enumerable, threshold, arg \\ [], opts) do
     {heads, tail} = Enum.split(enumerable, threshold)
 
-    split({mod, fun}, pid, id + 1, tail, threshold, opts)
-    ++ [{Process.spawn(mod, fun, [pid, heads, id], opts), id}]
+    split({mod, fun}, pid, id + 1, tail, threshold, arg, opts)
+    ++ [{Process.spawn(mod, fun, [pid, heads, id, arg], opts), id}]
   end
 end
